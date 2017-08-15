9f685d2d6dfdb1f44288d8435b3872a2f242fd83
William Bright <williambright91@gmail.com>
2017-08-14 21:16:25 -0400

    passed all tests after refork

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -2,4 +2,20 @@ import requests
 
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    r = requests.get('https://api.github.com/users/{username}/gists'.format(username=username))
+    if r.status_code == 200:
+        query = "INSERT INTO gists (github_id, html_url,git_pull_url,git_push_url, commits_url, forks_url, \
+            public, created_at, updated_at, comments, comments_url ) VALUES \
+            (:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, \
+                    :public, :created_at, :updated_at, :comments, :comments_url );"
+        for element in r.json():
+            cursors = db.execute(query,
+                {'github_id':element.get('id'), 'html_url':element.get('html_url'), 'git_pull_url':element.get('git_pull_url'),\
+                'git_push_url':element.get('git_push_url'), 'commits_url':element.get('commits_url'), 'forks_url':element.get('forks_url'), \
+                'public':element.get('public'), 'created_at':element.get('created_at'), 'updated_at':element.get('updated_at'), \
+                'comments':element.get('comments'), 'comments_url':element.get('comments_url')})
+
+        db.commit()
+    else:
+        r.raise_for_status()
+
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,43 @@
 from .models import Gist
+from datetime import datetime
+import datetime
 
 
 def search_gists(db_connection, **kwargs):
-    pass
+    operations={'gt': ">", 'gte': ">=", "lt": "<", 'lte': "<="}
+    c = db_connection.cursor()
+    g_list=[]
+    if kwargs:
+        for k in kwargs:
+            if k.find('created') > -1:
+                symbol = operations[k.split('__')[1]]
+                query_by = k.split('__')[0]
+                d=kwargs[k]
+                d=d.strftime("%Y-%m-%d")
+                #
+                #"SELECT * FROM gists WHERE {created_at} {symbol} '{date}';".format(created_at=query_by, symbol=symbol, date=d))
+
+                #"SELECT * FROM gists WHERE created_at <= '2014-05-04';"
+                if symbol == "<=":
+                    d=kwargs[k]
+                    d= d+datetime.timedelta(days=1)
+                    d=d.strftime("%Y-%m-%d")
+                c.execute("SELECT * FROM gists WHERE {created_at} {symbol} '{date}';".format(created_at=query_by, symbol=symbol, date=d))
+                for element in c.fetchall():
+                    g_list.append(Gist(element))
+        if 'github_id' in kwargs:
+            if not g_list:
+                c.execute("SELECT * FROM gists WHERE github_id ='{id}'".format(id=kwargs['github_id'])+ ";")
+                for e in c.fetchall():
+                    g_list.append(Gist(e))
+            else:
+                return [g for g in g_list if g.github_id==kwargs['github_id']]
+        return g_list
+
+    else:
+        c.execute('SELECT * FROM gists')
+        return c.fetchall()
+
+def build_query():
+    pass      
+
