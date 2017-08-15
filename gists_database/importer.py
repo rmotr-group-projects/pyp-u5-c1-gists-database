0eab9aae5e13fe1cf40a583b6145335f84d3fbab
Judith Jayawickrama <judithj@q.com>
2017-08-15 00:36:29 +0000

    Both importer and search are passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,41 @@
 import requests
+import sqlite3
+import json
 
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+
+    url='https://api.github.com/users/{}/gists'.format(username)
+
+    r = requests.get(url)
+    r.raise_for_status()
+    resp=r.json()     #list
+    
+    
+   
+    
+    
+    #get each gist as a dictionary from the list called resp. and populate db.
+    for element in resp:
+       
+       dict_for_db_element = {}
+    
+       for key, value in element.items():
+           
+          keys_wanted = ('id', 'html_url', 'git_pull_url', 'git_push_url', 'commits_url', 'forks_url', 'public', 'created_at',
+                         'updated_at', 'comments', 'comments_url')
+                         
+          json_dict = {db_key: element[db_key] for db_key in keys_wanted}
+    
+       dict_for_db_element.update(json_dict)
+    
+       db.execute("""INSERT INTO gists
+                  (github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url, public, created_at, updated_at, comments, comments_url)
+                  VALUES 
+                  (:id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, 
+                   :created_at, :updated_at,:comments, :comments_url)""", dict_for_db_element)
+                   
+     
+    db.commit()
+    
+    
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,48 @@
 from .models import Gist
-
+import sqlite3
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    
+    
+    
+   
+    # make filter for where clause
+    #map parms passed in kwargs to keys in database by making a dictionary {parms: key+operation...}
+    
+    map_parms_to_dbkey ={'github_id':'github_id =',   'public': 'public =',
+                         'created_at__gt': 'datetime(created_at) >', 'created_at__gte':'datetime(created_at) >=', 
+                         'created_at__lt': 'datetime(created_at) <', 'created_at__lte':'datetime(created_at) <=',
+                         'updated_at__gt': 'datetime(updated_at) >', 'updated_at__gte':'datetime(updated_at) >=', 
+                         'updated_at__lt':'datetime(updated_at ) <', 'updated_at__lte':'datetime(updated_at) <= ',
+                         'comments': 'comments ='}
+    
+    
+    if kwargs :
+        parms_value_dict={}
+        list_for_where_clause = []
+        for parms, value in kwargs.items():
+           parms_to_key = ('{} :{}'.format(map_parms_to_dbkey[parms], parms))
+           parms_value={parms : value}
+           parms_value_dict.update(parms_value)
+           list_for_where_clause.append(parms_to_key)
+
+        filter=' AND '.join(list_for_where_clause)
+    
+        select = 'Select * FROM gists WHERE {}'.format(filter)
+        
+        cursor=db_connection.execute(select, parms_value_dict)
+    else:
+        select=  'Select * FROM gists'
+        
+        cursor=db_connection.execute(select)
+        
+    
+    gists_found = cursor.fetchall()
+    
+    gists_list =[]
+    for gists in gists_found:
+        result= Gist(gists)
+        gists_list.append(result)
+    return gists_list
+        
