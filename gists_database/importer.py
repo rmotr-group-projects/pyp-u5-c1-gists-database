58f86b9ad69ede598a47b93d72d7824657fa206a
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-31 16:31:20 +0000

    All test passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,21 @@
 import requests
-
+import sqlite3
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    query = """INSERT INTO gists (
+    github_id, html_url, git_pull_url, git_push_url, commits_url, forks_url, 
+    public, created_at, updated_at, comments, comments_url
+    ) VALUES (
+    :id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url,
+    :public, :created_at, :updated_at, :comments, :comments_url
+    );"""
+    
+    r = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    r.raise_for_status()
+    all_gists = r.json()
+    
+    for gist in all_gists:
+        db.execute(query, gist)
+
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,35 @@
 from .models import Gist
-
+import sqlite3
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    query = build_query(**kwargs)
+        
+    cursor = db_connection.execute(query)
+    for row in cursor:
+        yield Gist(row)
+
+def build_query(**kwargs):
+    query = """select * from gists
+    where 1=1
+    """
+    
+    op_dict = {
+        'gt': ">",
+        'gte': '>=',
+        'lt': '<',
+        'lte': '<='
+    }
+    
+    for key, val in kwargs.items():
+        if isinstance(val,datetime):
+            op = key[-3:] if key[-1:] == 'e' else key[-2:]
+            field = key[0:-5] if key[-1:] == 'e' else key[0:-4]
+            operator = op_dict[op]
+            query = query + "and datetime({}) {} datetime('{}')\n".format(field, operator, val)
+        else:
+            if isinstance(val, str):
+                val = "'{}'".format(val)
+            query = query + 'and {} = {}\n'.format(key, val)
+    
+    return query
