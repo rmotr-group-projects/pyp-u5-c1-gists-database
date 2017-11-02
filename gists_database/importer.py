1558e14257a52ea2d6c690bc8b9e03f2141f1841
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-11-02 23:35:11 +0000

    working on importer

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,20 @@
 import requests
 
 
+API_ENDPOINT = 'https://api.github.com/users/{username}/gists'
+
+INSERT_GIST_QUERY = """INSERT INTO gists (
+    "github_id", "html_url", "git_pull_url",
+    "git_push_url", "commits_url",
+    "forks_url", "public", "created_at",
+    "updated_at", "comment", "comments_url"
+) Values (
+    :github_id, :html_url, :git_pull_url,
+    :git_push_url, :commits_url, :forks_url,
+);"""
+
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    gists_request = requests.get(API_ENDPOINT.format(username=username))
+#    gists = gists_request.json()
+#    db = sqlite3.connect(db)
