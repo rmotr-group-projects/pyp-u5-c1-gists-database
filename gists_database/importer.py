5ff0f6e8c5078c1a3e96a620a817db1abd538e48
Karis Shang <kyshang@gmail.com>
2017-11-06 02:40:48 +0000

    initial commit, tests passing

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,38 @@
 import requests
 
+API_ENDPOINT = 'https://api.github.com/users/{username}/gists'
+''
+INSERT_QUERY = """INSERT INTO gists(
+    "github_id", "html_url", "git_pull_url", 
+    "git_push_url", "commits_url", "forks_url", 
+    "public", "created_at", "updated_at", 
+    "comments", "comments_url" ) VALUES (
+    :github_id, :html_url, :git_pull_url, 
+    :git_push_url, :commits_url, :forks_url, 
+    :public, :created_at, :updated_at, 
+    :comments, :comments_url);"""
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = API_ENDPOINT.format(username = username)
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    gists = resp.json()
+    
+    for gist in gists:
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            'git_pull_url': gist['git_pull_url'],
+            'git_push_url': gist['git_push_url'],
+            'commits_url': gist['commits_url'],
+            'forks_url': gist['forks_url'],
+            'public': gist['public'],
+            'created_at': gist['created_at'],
+            'updated_at': gist['updated_at'],
+            'comments' : gist['comments'],
+            'comments_url': gist['comments_url'],
+        }
+        db.execute(INSERT_QUERY, params)
+        if commit:
+            db.commit()
