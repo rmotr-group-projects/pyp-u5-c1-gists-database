ab0022ca0d4aa62d23f39cf28756eb3c965571f8
Kelli Blalock <kelli.blalock2@gmail.com>
2017-11-08 23:18:34 +0000

    Pass test_search_with_github_id

--- gists_database/search.py
+++ gists_database/search.py
@@ -2,9 +2,12 @@ from models import Gist
 
 
 def search_gists(db_connection, **kwargs):
+    cur = db_connection.cursor()
     if len(kwargs) == 0:
-        cur = db_connection.cursor()
         cur.execute('SELECT * FROM gists')
-        for row in cur:
-             yield Gist(row)
-         
+    elif 'github_id' in kwargs:
+        cur.execute('SELECT * FROM gists WHERE github_id = :github_id',
+        { 'github_id': kwargs.get('github_id') })
+    
+    for row in cur:
+        yield Gist(row)
