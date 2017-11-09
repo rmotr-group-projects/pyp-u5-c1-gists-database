eb05fbbdb87a5255e494965c17e4da19efbc42f9
Kelli Blalock <kelli.blalock2@gmail.com>
2017-11-09 01:48:56 +0000

    Pass test_search_with_created_date_gt

--- gists_database/search.py
+++ gists_database/search.py
@@ -8,6 +8,9 @@ def search_gists(db_connection, **kwargs):
     elif 'github_id' in kwargs:
         cur.execute('SELECT * FROM gists WHERE github_id = :github_id',
         { 'github_id': kwargs.get('github_id') })
-    
+    elif 'created_at__gt' in kwargs:
+        cur.execute('SELECT * FROM gists WHERE datetime(created_at) >'
+                    'datetime(:date)', { 'date': kwargs.get('created_at__gt')})  
+        
     for row in cur:
         yield Gist(row)
