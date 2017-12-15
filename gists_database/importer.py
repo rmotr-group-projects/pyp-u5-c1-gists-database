a9f437fca994bf5875ef2002502a463e81b58a1e
William <luwp@hotmail.com>
2017-12-15 03:18:32 +0000

    1st commit

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,58 @@
 import requests
 
+INSERT_GIST_QUERY = """INSERT INTO gists (
+    "github_id", 
+    "html_url", 
+    "git_pull_url",
+    "git_push_url", 
+    "commits_url",
+    "forks_url", 
+    "public", 
+    "created_at",
+    "updated_at", 
+    "comments", 
+    "comments_url"
+) VALUES (
+    :github_id, 
+    :html_url, 
+    :git_pull_url,
+    :git_push_url, 
+    :commits_url, 
+    :forks_url,
+    :public, 
+    :created_at, 
+    :updated_at,
+    :comments, 
+    :comments_url
+);"""
+
+URL_TEMPLATE = 'https://api.github.com/users/{username}/gists'
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    resp = requests.get(URL_TEMPLATE.format(username=username))
+    resp.raise_for_status()
+    # resp = requests.get('https://api.github.com/users/'+ username + '/gists')
+
+    gists_data = resp.json() # list of dicts
+    # dict_keys(['url', 'forks_url', 'commits_url', 'id', 'git_pull_url', 'git_push_url', 
+    # 'html_url', 'files', 'public', 'created_at', 'updated_at', 'description', 'comments', 
+    # 'user', 'comments_url', 'owner', 'truncated'])
+    
+    for gist in gists_data:
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url'],
+        }
+        db.execute(INSERT_GIST_QUERY, params)
+        
+        if commit:
+            db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,67 @@
 from .models import Gist
 
+# sql syntax example  
+# d = datetime(2017, 5, 10)
+# cursor = db.execute('SELECT * FROM gists WHERE created_at > datetime(:created_at)', {'created_at': d})
 
-def search_gists(db_connection, **kwargs):
-    pass
+class search_gists(object):
+    # parse key string to column and operator
+    OPERATION_STRING = {
+        'lt': '<',
+        'lte': '<=',
+        'gt': '>',
+        'gte': '>='
+    }
+    
+    # datatime columns
+    DATETIME_COLUMNS = ('created_at', 'updated_at')
+    
+    # parse string to column + operator
+    def _parse(self, key_string):
+        if '__' in key_string:
+            column, op_string = key_string.split('__')
+            op = self.__class__.OPERATION_STRING[op_string]
+        else:
+            column = key_string
+            op = '='
+        return column, op   
+        
+    # check if the column is datetime type. Datetime column has different sql syntax
+    def is_datetime(self, column):
+        if column in self.__class__.DATETIME_COLUMNS:
+            return True
+        return False
+        
+    # initialization, create the db cursor
+    def __init__(self, db_connection, **kwargs):
+        db_connection = db_connection
+        query = 'SELECT * FROM gists'
+        values = {}
+        if kwargs:
+            filters = []
+            for key_string, value in kwargs.items():
+                column, op = self._parse(key_string)
+                
+                if self.is_datetime(column):
+                    filters.append('datetime(%s) %s datetime(:%s)' % (column, op, key_string))
+                else :
+                    filters.append('%s %s :%s' % (column, op, key_string))
+                    
+                values[key_string] = value
+
+            query += ' WHERE '
+            query += ' AND '.join(filters) 
+        self.cursor =db_connection.execute(query, values)
+        
+    # iteratable
+    def __iter__(self):
+        return self
+        
+    # iterator
+    def __next__(self):
+        result = self.cursor.fetchone()
+        if result is None:
+            raise StopIteration()
+        return Gist(result) 
+
+    next = __next__ 
