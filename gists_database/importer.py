ad43f480dbfe73d24a2bf4fa2ead3a25e3ba83d3
Alara Dirik <alaradirik@gmail.com>
2017-12-18 17:12:18 +0000

    Done

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,30 @@
 import requests
+import json
+import sqlite3
 
-
+INSERT = """INSERT INTO gists (github_id, html_url, git_pull_url, git_push_url, commits_url, 
+forks_url, public, created_at, updated_at, comments, comments_url) VALUES  
+(:github_id, :html_url, :git_pull_url, :git_push_url, :commits_url, 
+:forks_url, :public, :created_at, :updated_at, :comments, :comments_url)"""
+ 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    response = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    response.raise_for_status()
+    data_request = response.json()
+    
+    curs = db.cursor()
+    for gist in data_request:
+        gist_params = {"url" : gist["url"],
+        "github_id": gist["id"], 
+        "html_url": gist["html_url"], 
+        "git_pull_url": gist["git_pull_url"],
+        "git_push_url": gist["git_push_url"], 
+        "commits_url": gist["commits_url"],
+        "forks_url": gist["forks_url"],
+        "public": gist["public"], 
+        "created_at": gist["created_at"],
+        "updated_at": gist["updated_at"],
+        "comments": gist["comments"],
+        "comments_url": gist["comments_url"]}
+        curs.execute(INSERT, gist_params)
+    db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,26 @@
 from .models import Gist
-
+from datetime import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    dict = {"gt":">", "gte":">=", "lt": "<", "lte":"<=", "=": "="}
+    search_query="SELECT * from gists"
+    if kwargs:
+        search_query+=" WHERE "
+        multiple_search_cond = False
+        for k, v in kwargs.items():
+            attr, _, comparison = k.partition("__")
+            if not comparison:
+                comparison = "="
+            if multiple_search_cond:
+                search_query+=" AND "
+            if attr in ("created_at", "updated_at"):
+                search_query+="datetime(" + attr + ")" + dict[comparison] + "datetime('" + str(v) +"')"
+            else:
+                search_query+=attr + dict[comparison] + "'" + str(v) +"'"
+            multiple_search_cond = True
+            
+    print(search_query)
+    curs = db_connection.cursor()
+    curs.execute(search_query)
+    for res in curs:
+        yield Gist(res)
