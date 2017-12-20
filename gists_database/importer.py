efd771f11c8cb7d87a85c196c61a9193b53264b2
unoor <usman.noor127@hotmail.com>
2017-12-20 05:45:59 +0000

    Importer + Search

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -15,9 +15,6 @@ def import_gists_to_database(db, username, commit=True):
         #r.raise_for_status()
         raise requests.exceptions.HTTPError
         
-    
- 
-    
     query = """INSERT INTO gists (github_id,html_url,git_pull_url,git_push_url,commits_url,forks_url,public,created_at,updated_at,comments,comments_url) 
             VALUES (:id,:html_url,:git_pull_url,:git_push_url,:commits_url,:forks_url,:public,:created_at,:updated_at,:comments,:comments_url        
             );"""
@@ -31,15 +28,15 @@ def import_gists_to_database(db, username, commit=True):
         
 
 # #pprint.pprint(json_file[0]['id'])
-# db = sqlite3.connect('schema.db')
-# username = 'gvanrossum1'
-# import_gists_to_database(db, username)
-# cursor = db.cursor()
-# results = db.execute('SELECT * FROM gists')
-# print("gists:")
-# print(results.fetchone())
-# # for row in results:
-# #     print row
+db = sqlite3.connect('schema.db')
+username = 'gvanrossum'
+import_gists_to_database(db, username)
+cursor = db.cursor()
+results = db.execute('SELECT * FROM gists')
+print("gists:")
+#print(results.fetchone())
+for row in results:
+    print row
 
 
 
