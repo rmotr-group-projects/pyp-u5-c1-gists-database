3ef891b57a3b15a85820c94c749ee3e66744174a
Jaroslaw Truchan <jaroslaw.truchan@gmail.com>
2017-12-25 14:59:10 +0000

    Gists project working

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,32 @@
 import requests
+import sqlite3
+from .models import Gist
 
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    r = requests.get('https://api.github.com/users/%s/gists' % username)
+    r.raise_for_status()
+    
+    query = "INSERT INTO gists (github_id,html_url,git_pull_url,git_push_url,\
+        commits_url,forks_url,public,created_at,updated_at,comments,comments_url)\
+        VALUES (:github_id,:html_url,:git_pull_url,:git_push_url,:commits_url,\
+        :forks_url,:public,:created_at,:updated_at,:comments,:comments_url)"
+    
+    for data in r.json():
+        db.execute(query, {
+           'github_id': data['id'],
+           'html_url': data['html_url'],
+           'git_pull_url': data['git_pull_url'],
+           'git_push_url': data['git_push_url'],
+           'commits_url': data['commits_url'],
+           'forks_url': data['forks_url'],
+           'public': data['public'],
+           'created_at': data['created_at'],
+           'updated_at': data['updated_at'],
+           'comments': data['comments'],
+           'comments_url': data['comments_url']
+           })
+    
+    if commit:
+        db.commit()
+    
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,25 @@
 from .models import Gist
+from datetime import datetime
 
 
 def search_gists(db_connection, **kwargs):
-    pass
+    dict = {"gt":">", "gte":">=", "lt":"<", "lte":"<=", "=":"="}
+    search_query = "SELECT * FROM gists"
+    
+    if kwargs:
+        search_query += " WHERE "
+        more_cond = 0
+        for k, v in kwargs.items():
+            attr, _, comparison = k.partition("__")
+            if more_cond > 0: #if non-first run over kwargs
+                search_query += " AND "
+            if attr in ("created_at", "updated_at"):
+                search_query += "datetime(" + attr + ")" + dict[comparison] + "datetime('" + str(v) + "')"
+            else:
+                search_query += attr + " = " + "'" + str(v) + "'"
+            if len(kwargs) > 1: more_cond += 1
+    
+    cur = db_connection.cursor()
+    cur.execute(search_query)
+    for result in cur:
+        yield Gist(result)
